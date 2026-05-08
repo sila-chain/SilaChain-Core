47bd2b05dd4661964de1d95fc124b42c0f498889
AHMED IBRAHIM <ahmed@example.com>
2026-05-08 22:59:53 +0300

    cmd/sila: add initial command bootstrap

---
+++ cmd/sila/main.go
@@ -0,0 +1,12 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+//
+// The SilaChain library is derived from the go-ethereum library.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("SilaChain command bootstrap is not wired yet")
+}
