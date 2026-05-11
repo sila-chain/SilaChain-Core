aee289df1af71a54033f2cfa6bf1162e8536b0b4
AHMED IBRAHIM <ahmed@example.com>
2026-05-11 17:09:40 +0300

    cmd/sila: fold command action into main

--- cmd/sila/main.go
+++ cmd/sila/main.go
@@ -8,8 +8,13 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
+func runSilaCommand(ctx *cli.Context) error {
+	return runSilaRuntime(ctx)
+}
 func main() {
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
