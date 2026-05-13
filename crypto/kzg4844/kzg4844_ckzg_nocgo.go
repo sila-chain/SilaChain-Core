10acdbe66d2736bd8a4bc3d57ef8b676546db9b7
AHMED IBRAHIM <ahmed@example.com>
2026-05-13 20:12:40 +0300

    crypto: update headers to SilaChain

--- crypto/kzg4844/kzg4844_ckzg_nocgo.go
+++ crypto/kzg4844/kzg4844_ckzg_nocgo.go
@@ -1,18 +1,18 @@
-// Copyright 2023 The go-ethereum Authors
-// This file is part of the SilaChain library (derived from go-ethereum).
+// Copyright 2023 The SilaChain Authors
+// This file is part of the SilaChain library.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
+// The SilaChain library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The SilaChain library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// along with the SilaChain library. If not, see <http://www.gnu.org/licenses/>.
 
 //go:build !ckzg || nacl || js || wasip1 || !cgo || gofuzz
 
@@ -20,7 +20,7 @@ package kzg4844
 
 import "sync"
 
-// ckzgAvailable signals whether the library was compiled into Geth.
+// ckzgAvailable signals whether the library was compiled into SilaChain.
 const ckzgAvailable = false
 
 // ckzgIniter ensures that we initialize the KZG library once before using it.
