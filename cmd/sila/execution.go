47c667b4d99ac2c5f64f281872b87a3889574aec
AHMED IBRAHIM <ahmed@example.com>
2026-05-15 23:36:29 +0300

    cmd/sila: merge API and build info runtime helpers

--- cmd/sila/execution.go
+++ cmd/sila/execution.go
@@ -5,6 +5,10 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
+	"strings"
+
 	"github.com/sila-org/sila/beacon/blsync"
 	bparams "github.com/sila-org/sila/beacon/params"
 	"github.com/sila-org/sila/cmd/utils"
@@ -12,6 +16,9 @@ import (
 	"github.com/sila-org/sila/eth"
 	"github.com/sila-org/sila/eth/catalyst"
 	ethconfig "github.com/sila-org/sila/eth/ethconfig"
+	"github.com/sila-org/sila/eth/filters"
+	ethapi "github.com/sila-org/sila/internal/ethapi"
+	"github.com/sila-org/sila/metrics"
 	"github.com/sila-org/sila/node"
 	"github.com/sila-org/sila/rpc"
 )
@@ -63,4 +70,36 @@ func ConfigureConsensusRuntime(
 	}
 
 	return RegisterEngineAPI(stack, ethBackend)
+}
+
+// RegisterBuildInfoGauge creates gauge with SilaChain system and build information.
+func RegisterBuildInfoGauge(ethBackend *eth.Ethereum, version string) {
+	if ethBackend == nil {
+		return
+	}
+	var protos []string
+	for _, p := range ethBackend.Protocols() {
+		protos = append(protos, fmt.Sprintf("%v/%d", p.Name, p.Version))
+	}
+	metrics.NewRegisteredGaugeInfo("sila/info", nil).Update(metrics.GaugeInfoValue{
+		"arch":      runtime.GOARCH,
+		"os":        runtime.GOOS,
+		"version":   version,
+		"protocols": strings.Join(protos, ","),
+	})
+}
+
+// RegisterFilterAPI configures the log filter RPC API.
+func RegisterFilterAPI(stack *node.Node, backend ethapi.Backend, cfg *ethconfig.Config) *filters.FilterSystem {
+	return utils.RegisterFilterAPI(stack, backend, cfg)
+}
+
+// RegisterGraphQLService configures GraphQL if requested.
+func RegisterGraphQLService(stack *node.Node, backend ethapi.Backend, filterSystem *filters.FilterSystem, cfg *node.Config) {
+	utils.RegisterGraphQLService(stack, backend, filterSystem, cfg)
+}
+
+// RegisterEthStatsService adds the Sila stats daemon if requested.
+func RegisterEthStatsService(stack *node.Node, backend *eth.EthAPIBackend, url string) {
+	utils.RegisterEthStatsService(stack, backend, url)
 }
