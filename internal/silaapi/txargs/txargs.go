0130397b7c1384f815decba7444bd02b040048c4
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 16:27:03 +0300

    internal/silaapi: introduce transaction args boundary

---
+++ internal/silaapi/txargs/txargs.go
@@ -0,0 +1,8 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package txargs
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+type TransactionArgs = ethapi.TransactionArgs
