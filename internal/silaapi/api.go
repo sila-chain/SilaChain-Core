2347cf652d79fa131214bbc84eda4374926c3bb0
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 15:55:57 +0300

    internal/silaapi: introduce api boundary

---
+++ internal/silaapi/api.go
@@ -0,0 +1,17 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package silaapi
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+type Backend = ethapi.Backend
+type TransactionArgs = ethapi.TransactionArgs
+type NetAPI = ethapi.NetAPI
+
+var GetAPIs = ethapi.GetAPIs
+var NewNetAPI = ethapi.NewNetAPI
+var RPCMarshalBlock = ethapi.RPCMarshalBlock
+var DoCall = ethapi.DoCall
+var DoEstimateGas = ethapi.DoEstimateGas
+var SubmitTransaction = ethapi.SubmitTransaction
