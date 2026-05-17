b1e8fd31f3a27a661c150024559fad3bdeddbed5
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 16:43:34 +0300

    internal/silaapi: introduce net api boundary

---
+++ internal/silaapi/netapi/netapi.go
@@ -0,0 +1,10 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package netapi
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+type NetAPI = ethapi.NetAPI
+
+var NewNetAPI = ethapi.NewNetAPI
