9f3f0a2c99d1c44c3b1c625939c818cb0fe73088
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 17:02:06 +0300

    internal/silaapi: introduce call api boundary

---
+++ internal/silaapi/callapi/callapi.go
@@ -0,0 +1,10 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package callapi
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+var DoCall = ethapi.DoCall
+var DoEstimateGas = ethapi.DoEstimateGas
+var SubmitTransaction = ethapi.SubmitTransaction
