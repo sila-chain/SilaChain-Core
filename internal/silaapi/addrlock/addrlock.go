0c464ac206c46ab2e3537d04dfda792bef2167b5
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 17:19:49 +0300

    internal/silaapi: introduce address lock boundary

---
+++ internal/silaapi/addrlock/addrlock.go
@@ -0,0 +1,8 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package addrlock
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+type AddrLocker = ethapi.AddrLocker
