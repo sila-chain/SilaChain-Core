f3423815b0ccbdd22102b79034dd41ecf6ec0960
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 16:21:43 +0300

    internal/silaapi: introduce override boundary

---
+++ internal/silaapi/override/override.go
@@ -0,0 +1,10 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package override
+
+import ethoverride "github.com/sila-org/sila/internal/ethapi/override"
+
+type OverrideAccount = ethoverride.OverrideAccount
+type StateOverride = ethoverride.StateOverride
+type BlockOverrides = ethoverride.BlockOverrides
