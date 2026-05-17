966ac0c1d0dfa87d599c0e71789f3475484163ec
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 17:05:11 +0300

    internal/silaapi: introduce registry boundary

---
+++ internal/silaapi/registry/registry.go
@@ -0,0 +1,8 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package registry
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+var GetAPIs = ethapi.GetAPIs
