56d8d58940d8fc929d26ad9cf462990df50828ce
AHMED IBRAHIM <ahmed@example.com>
2026-05-18 20:58:51 +0300

    rpc: expose sila engine namespace

--- node/defaults.go
+++ node/defaults.go
@@ -50,7 +50,7 @@ var (
 	DefaultAuthVhosts  = []string{"localhost"} // Default virtual hosts for the authenticated apis
 	DefaultAuthOrigins = []string{"localhost"} // Default origins for the authenticated apis
 	DefaultAuthPrefix  = ""                    // Default prefix for the authenticated apis
-	DefaultAuthModules = []string{"eth", "silaNet", "silaWeb3", "engine"}
+	DefaultAuthModules = []string{"eth", "silaNet", "silaWeb3", "engine", "silaEngine"}
 )
 
 // DefaultConfig contains reasonable default settings.
