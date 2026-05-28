671be242e177f55cd9e8a515bb2ac5447f5e3c67
AHMED IBRAHIM <ahmed@example.com>
2026-05-28 14:34:39 +0300

    core: add Sila PoW genesis test config helper

--- core/genesis_test.go
+++ core/genesis_test.go
@@ -34,6 +34,9 @@ import (
 	"github.com/sila-org/sila/triedb/pathdb"
 )
 
+func newSilaPoWTestConfig(homestead *big.Int) *params.ChainConfig {
+	return &params.ChainConfig{HomesteadBlock: homestead, Ethash: &params.SilaPoWConfig{}}
+}
 func TestSetupGenesis(t *testing.T) {
 	testSetupGenesis(t, rawdb.HashScheme)
 	testSetupGenesis(t, rawdb.PathScheme)
@@ -43,14 +46,14 @@ func testSetupGenesis(t *testing.T, scheme string) {
 	var (
 		customghash = common.HexToHash("0x89c99d90b79719238d2645c7642f2c9295246e80775b38cfd162b696817fbd50")
 		customg     = Genesis{
-			Config: &params.ChainConfig{HomesteadBlock: big.NewInt(3), Ethash: &params.SilaPoWConfig{}},
+			Config: newSilaPoWTestConfig(big.NewInt(3)),
 			Alloc: types.GenesisAlloc{
 				{1}: {Balance: big.NewInt(1), Storage: map[common.Hash]common.Hash{{1}: {1}}},
 			},
 		}
 		oldcustomg = customg
 	)
-	oldcustomg.Config = &params.ChainConfig{HomesteadBlock: big.NewInt(2), Ethash: &params.SilaPoWConfig{}}
+	oldcustomg.Config = newSilaPoWTestConfig(big.NewInt(2))
 
 	tests := []struct {
 		name           string
