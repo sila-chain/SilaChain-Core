3bd2ef6420acf66ef99a73b77ac0257109483691
AHMED IBRAHIM <ahmed@example.com>
2026-05-29 11:57:26 +0300

    build: update abigen description to Sila

--- build/ci.go
+++ build/ci.go
@@ -133,7 +133,7 @@ var (
 	debExecutables = []debExecutable{
 		{
 			BinaryName:  "abigen",
-			Description: "Source code generator to convert Ethereum contract definitions into easy to use, compile-time type-safe Go packages.",
+			Description: "Source code generator to convert Sila contract definitions into easy to use, compile-time type-safe Go packages.",
 		},
 		{
 			BinaryName:  "evm",
