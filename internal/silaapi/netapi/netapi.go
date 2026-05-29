6f64925364d06064bc2b8a5f9af5e589964527ca
AHMED IBRAHIM <ahmed@example.com>
2026-05-29 22:59:44 +0300

    internal/silaapi/netapi: own network API

--- internal/silaapi/netapi/netapi.go
+++ internal/silaapi/netapi/netapi.go
@@ -3,8 +3,35 @@
 
 package netapi
 
-import ethapi "github.com/sila-org/sila/internal/ethapi"
+import (
+	"fmt"
 
-type NetAPI = ethapi.NetAPI
+	"github.com/sila-org/sila/common/hexutil"
+	"github.com/sila-org/sila/p2p"
+)
 
-var NewNetAPI = ethapi.NewNetAPI
+// NetAPI offers Sila network related RPC methods.
+type NetAPI struct {
+	net            *p2p.Server
+	networkVersion uint64
+}
+
+// NewNetAPI creates a new Sila net API instance.
+func NewNetAPI(net *p2p.Server, networkVersion uint64) *NetAPI {
+	return &NetAPI{net, networkVersion}
+}
+
+// Listening returns an indication if the node is listening for network connections.
+func (api *NetAPI) Listening() bool {
+	return true // always listening
+}
+
+// PeerCount returns the number of connected peers.
+func (api *NetAPI) PeerCount() hexutil.Uint {
+	return hexutil.Uint(api.net.PeerCount())
+}
+
+// Version returns the current legacy-compatible network protocol version.
+func (api *NetAPI) Version() string {
+	return fmt.Sprintf("%d", api.networkVersion)
+}
