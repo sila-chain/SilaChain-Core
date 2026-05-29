d4d38fd7514cbfde4d28c6b236dd776fc003f290
AHMED IBRAHIM <ahmed@example.com>
2026-05-29 17:15:51 +0300

    cmd/clef: use Sila transaction result API boundary

---
+++ internal/silaapi/tx_result.go
@@ -0,0 +1,8 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package silaapi
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+type SignTransactionResult = ethapi.SignTransactionResult
