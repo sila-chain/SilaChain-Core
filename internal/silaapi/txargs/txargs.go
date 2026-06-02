04a5dc45477afb45787ee205f64db3f4e5032de2
AHMED IBRAHIM <ahmed@example.com>
2026-06-02 17:51:26 +0300

    internal/silaapi/txargs: add transaction args call helpers

--- internal/silaapi/txargs/txargs.go
+++ internal/silaapi/txargs/txargs.go
@@ -3,6 +3,19 @@
 
 package txargs
 
-import ethapi "github.com/sila-org/sila/internal/ethapi"
+import (
+	"math/big"
+
+	"github.com/sila-org/sila/core"
+	ethapi "github.com/sila-org/sila/internal/ethapi"
+)
 
 type TransactionArgs = ethapi.TransactionArgs
+
+func CallDefaults(args *TransactionArgs, globalGasCap uint64, baseFee *big.Int, chainID *big.Int) error {
+	return args.CallDefaults(globalGasCap, baseFee, chainID)
+}
+
+func ToMessage(args *TransactionArgs, baseFee *big.Int, skipNonceCheck bool) *core.Message {
+	return args.ToMessage(baseFee, skipNonceCheck)
+}
