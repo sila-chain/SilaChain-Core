75e286b74f11e2e081e2c094e4543e21bf4c1221
AHMED IBRAHIM <ahmed@example.com>
2026-06-03 18:53:26 +0300

    internal/silaapi: wrap registry blockchain transaction APIs

--- internal/silaapi/registry/registry.go
+++ internal/silaapi/registry/registry.go
@@ -11,15 +11,23 @@ import (
 	"github.com/sila-org/sila/rpc"
 )
 
+func NewSilaBlockChainAPI(apiBackend backend.Backend) interface{} {
+	return ethapi.NewSilaBlockChainAPI(apiBackend)
+}
+
+func NewSilaTransactionAPI(apiBackend backend.Backend, nonceLock *addrlock.AddrLocker) interface{} {
+	return ethapi.NewSilaTransactionAPI(apiBackend, nonceLock)
+}
+
 func GetAPIs(apiBackend backend.Backend) []rpc.API {
 	nonceLock := new(addrlock.AddrLocker)
 	return []rpc.API{
 		{Namespace: "sila", Service: silaapi.NewSilaAPI(apiBackend)},
 		{Namespace: "eth", Service: silaapi.NewSilaAPI(apiBackend)},
-		{Namespace: "sila", Service: ethapi.NewSilaBlockChainAPI(apiBackend)},
-		{Namespace: "eth", Service: ethapi.NewSilaBlockChainAPI(apiBackend)},
-		{Namespace: "sila", Service: ethapi.NewSilaTransactionAPI(apiBackend, nonceLock)},
-		{Namespace: "eth", Service: ethapi.NewSilaTransactionAPI(apiBackend, nonceLock)},
+		{Namespace: "sila", Service: NewSilaBlockChainAPI(apiBackend)},
+		{Namespace: "eth", Service: NewSilaBlockChainAPI(apiBackend)},
+		{Namespace: "sila", Service: NewSilaTransactionAPI(apiBackend, nonceLock)},
+		{Namespace: "eth", Service: NewSilaTransactionAPI(apiBackend, nonceLock)},
 		{Namespace: "txpool", Service: silaapi.NewTxPoolAPI(apiBackend)},
 		{Namespace: "debug", Service: silaapi.NewDebugAPI(apiBackend)},
 		{Namespace: "sila", Service: silaapi.NewSilaAccountAPI(apiBackend.AccountManager())},
