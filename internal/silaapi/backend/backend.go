c06e93f259f8e1948a7e23df7b1165c0c5b3cc1f
AHMED IBRAHIM <ahmed@example.com>
2026-06-06 16:51:15 +0300

    internal/silaapi/backend: define backend interface

--- internal/silaapi/backend/backend.go
+++ internal/silaapi/backend/backend.go
@@ -3,6 +3,84 @@
 
 package backend
 
-import ethapi "github.com/sila-org/sila/internal/ethapi"
+import (
+	"context"
+	"math/big"
+	"time"
 
-type Backend = ethapi.Backend
+	"github.com/sila-org/sila"
+	"github.com/sila-org/sila/accounts"
+	"github.com/sila-org/sila/common"
+	"github.com/sila-org/sila/consensus"
+	"github.com/sila-org/sila/core"
+	"github.com/sila-org/sila/core/filtermaps"
+	"github.com/sila-org/sila/core/state"
+	"github.com/sila-org/sila/core/types"
+	"github.com/sila-org/sila/core/vm"
+	"github.com/sila-org/sila/ethdb"
+	"github.com/sila-org/sila/event"
+	"github.com/sila-org/sila/params"
+	"github.com/sila-org/sila/rpc"
+)
+
+type Backend interface {
+	// General SilaChain API
+	SyncProgress(ctx context.Context) ethereum.SyncProgress
+
+	SuggestGasTipCap(ctx context.Context) (*big.Int, error)
+	FeeHistory(ctx context.Context, blockCount uint64, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*big.Int, [][]*big.Int, []*big.Int, []float64, []*big.Int, []float64, error)
+	BlobBaseFee(ctx context.Context) *big.Int
+	ChainDb() ethdb.Database
+	AccountManager() *accounts.Manager
+	ExtRPCEnabled() bool
+	RPCGasCap() uint64
+	RPCEVMTimeout() time.Duration
+	RPCTxFeeCap() float64
+	UnprotectedAllowed() bool
+	RPCTxSyncDefaultTimeout() time.Duration
+	RPCTxSyncMaxTimeout() time.Duration
+
+	// Blockchain API
+	SetHead(number uint64)
+	HeaderByNumber(ctx context.Context, number rpc.BlockNumber) (*types.Header, error)
+	HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error)
+	HeaderByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*types.Header, error)
+	CurrentHeader() *types.Header
+	CurrentBlock() *types.Header
+	BlockByNumber(ctx context.Context, number rpc.BlockNumber) (*types.Block, error)
+	BlockByHash(ctx context.Context, hash common.Hash) (*types.Block, error)
+	BlockByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*types.Block, error)
+	StateAndHeaderByNumber(ctx context.Context, number rpc.BlockNumber) (*state.StateDB, *types.Header, error)
+	StateAndHeaderByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*state.StateDB, *types.Header, error)
+	Pending() (*types.Block, types.Receipts, *state.StateDB)
+	GetReceipts(ctx context.Context, hash common.Hash) (types.Receipts, error)
+	GetCanonicalReceipt(tx *types.Transaction, blockHash common.Hash, blockNumber, blockIndex uint64) (*types.Receipt, error)
+	GetEVM(ctx context.Context, state *state.StateDB, header *types.Header, vmConfig *vm.Config, blockCtx *vm.BlockContext) *vm.EVM
+	SubscribeChainEvent(ch chan<- core.ChainEvent) event.Subscription
+	SubscribeChainHeadEvent(ch chan<- core.ChainHeadEvent) event.Subscription
+
+	// Transaction pool API
+	SendTx(ctx context.Context, signedTx *types.Transaction) error
+	GetCanonicalTransaction(txHash common.Hash) (bool, *types.Transaction, common.Hash, uint64, uint64)
+	TxIndexDone() bool
+	GetPoolTransactions() (types.Transactions, error)
+	GetPoolTransaction(txHash common.Hash) *types.Transaction
+	GetPoolNonce(ctx context.Context, addr common.Address) (uint64, error)
+	Stats() (pending int, queued int)
+	TxPoolContent() (map[common.Address][]*types.Transaction, map[common.Address][]*types.Transaction)
+	TxPoolContentFrom(addr common.Address) ([]*types.Transaction, []*types.Transaction)
+	SubscribeNewTxsEvent(chan<- core.NewTxsEvent) event.Subscription
+
+	ChainConfig() *params.ChainConfig
+	Engine() consensus.Engine
+	HistoryPruningCutoff() uint64
+
+	// This is copied from filters.Backend
+	GetBody(ctx context.Context, hash common.Hash, number rpc.BlockNumber) (*types.Body, error)
+	GetLogs(ctx context.Context, blockHash common.Hash, number uint64) ([][]*types.Log, error)
+	SubscribeRemovedLogsEvent(ch chan<- core.RemovedLogsEvent) event.Subscription
+	SubscribeLogsEvent(ch chan<- []*types.Log) event.Subscription
+
+	CurrentView() *filtermaps.ChainView
+	NewMatcherBackend() filtermaps.MatcherBackend
+}
