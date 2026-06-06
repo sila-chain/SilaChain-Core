4b39d6532c163424c43fc0c81d94de1e70880eae
AHMED IBRAHIM <ahmed@example.com>
2026-06-06 21:58:01 +0300

    internal/silaapi/registry: remove eth namespace registration

--- internal/silaapi/registry/registry.go
+++ internal/silaapi/registry/registry.go
@@ -50,14 +50,10 @@ func GetAPIs(apiBackend backend.Backend) []rpc.API {
 	nonceLock := new(addrlock.AddrLocker)
 	return []rpc.API{
 		{Namespace: "sila", Service: silaapi.NewSilaAPI(apiBackend)},
-		{Namespace: "eth", Service: silaapi.NewSilaAPI(apiBackend)},
 		{Namespace: "sila", Service: NewSilaBlockChainAPI(apiBackend)},
-		{Namespace: "eth", Service: NewSilaBlockChainAPI(apiBackend)},
 		{Namespace: "sila", Service: NewSilaTransactionAPI(apiBackend, nonceLock)},
-		{Namespace: "eth", Service: NewSilaTransactionAPI(apiBackend, nonceLock)},
 		{Namespace: "txpool", Service: silaapi.NewTxPoolAPI(apiBackend)},
 		{Namespace: "debug", Service: silaapi.NewDebugAPI(apiBackend)},
 		{Namespace: "sila", Service: silaapi.NewSilaAccountAPI(apiBackend.AccountManager())},
-		{Namespace: "eth", Service: silaapi.NewSilaAccountAPI(apiBackend.AccountManager())},
 	}
 }
