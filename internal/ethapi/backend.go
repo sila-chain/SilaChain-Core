e1f891a11abbee16362f04f9354c37284e8a9300
AHMED IBRAHIM <ahmed@example.com>
2026-06-06 23:30:28 +0300

    internal/silaapi: remove ethapi compatibility layer

--- internal/ethapi/backend.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 The SilaChain Authors
-// This file is part of the SilaChain library.
-
-// Package ethapi preserves compatibility wrappers for the SilaChain API.
-package ethapi
-
-import silabackend "github.com/sila-org/sila/internal/silaapi/backend"
-
-type Backend = silabackend.Backend
