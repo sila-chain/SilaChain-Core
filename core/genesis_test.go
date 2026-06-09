662564a0b99c5748e588795442fda9919f5ead50
AHMED IBRAHIM <ahmed@example.com>
2026-06-09 14:47:41 +0300

    core: add Sila genesis nil setup case

--- core/genesis_test.go
+++ core/genesis_test.go
@@ -81,6 +81,15 @@ func testSetupGenesis(t *testing.T, scheme string) {
 			wantConfig: params.MainnetChainConfig,
 		},
 		{
+			name: "sila block in DB, genesis == nil",
+			fn: func(db ethdb.Database) (*params.ChainConfig, common.Hash, *params.ConfigCompatError, error) {
+				SilaDefaultGenesisBlock().MustCommit(db, triedb.NewDatabase(db, newDbConfig(scheme)))
+				return SetupGenesisBlock(db, triedb.NewDatabase(db, newDbConfig(scheme)), nil)
+			},
+			wantHash:   params.SilaMainnetGenesisHash,
+			wantConfig: params.SilaMainnetChainConfig,
+		},
+		{
 			name: "mainnet block in DB, genesis == nil",
 			fn: func(db ethdb.Database) (*params.ChainConfig, common.Hash, *params.ConfigCompatError, error) {
 				DefaultGenesisBlock().MustCommit(db, triedb.NewDatabase(db, newDbConfig(scheme)))
