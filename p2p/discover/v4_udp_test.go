850f001a20bb8d1708897141f0a4e258e60da128
Ahmed Sila Local <ahmed@sila.local>
2026-06-28 17:33:04 +0300

    chore: rename remaining protocol identity references

--- p2p/discover/v4_udp_test.go
+++ p2p/discover/v4_udp_test.go
@@ -471,7 +471,7 @@ func TestUDPv4_successfulPing(t *testing.T) {
 }
 
 // This test checks that SIP-868 requests work.
-func TestUDPv4_EIP868(t *testing.T) {
+func TestUDPv4_SIP868(t *testing.T) {
 	test := newUDPTest(t)
 	defer test.close()
 
